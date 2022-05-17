cb8bd777e95eae6fef05824df403e0dd8c8c5058
ShikiSuen <shikisuen@outlook.com>
2022-05-17 17:29:39 +0800

    Add walk() and refactor certain functions in Grid. (#23)
    * Main // Deprecation. We use MegrezTests.swift instead.
    
    * Grid // Refactor certain functions.
    
    * Walker // Add walk() with test cases.

--- Tests/MegrezTests/MegrezTests.swift
+++ Tests/MegrezTests/MegrezTests.swift
@@ -60,7 +60,7 @@ final class MegrezTests: XCTestCase {
 
     let walker = Megrez.Walker(grid: builder.grid())
 
-    var walked = walker.reverseWalk(at: builder.grid().width(), score: 0.0, nodesLimit: 3, balanced: true).reversed()
+    var walked = walker.walk(at: builder.grid().width(), score: 0.0, nodesLimit: 3, balanced: true)
 
     // 這裡模擬一個輸入法的常見情況：每次敲一個字詞都會 walk，然後你回頭編輯完一些內容之後又會立刻重新 walk。
     // 如果只在這裡測試第一遍 walk 的話，測試通過了也無法測試之後再次 walk 是否會正常。
@@ -69,14 +69,14 @@ final class MegrezTests: XCTestCase {
     builder.deleteReadingToTheFrontOfCursor()
 
     // 於是咱們 walk 第二遍
-    walked = walker.reverseWalk(at: builder.grid().width(), score: 0.0, nodesLimit: 3, balanced: true).reversed()
+    walked = walker.walk(at: builder.grid().width(), score: 0.0, nodesLimit: 3, balanced: true)
     XCTAssert(!walked.isEmpty)
 
     // 做好第三遍的準備，這次咱們來一次插入性編輯
     builder.insertReadingAtCursor(reading: "ke1")  // 重點測試這句是否正常，畢竟是在 walked 過的節點內進行插入編輯
 
     // 於是咱們 walk 第三遍，這一遍會直接曝露「上述修改是否有對 builder 造成了破壞性的損失」所以很重要
-    walked = walker.reverseWalk(at: builder.grid().width(), score: 0.0, nodesLimit: 3, balanced: true).reversed()
+    walked = walker.walk(at: builder.grid().width(), score: 0.0, nodesLimit: 3, balanced: true)
     XCTAssert(!walked.isEmpty)
 
     var composed: [String] = []

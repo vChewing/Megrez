925d1d90e53b46d12def5c093a3a5e3d64adebf9
ShikiSuen <shikisuen@outlook.com>
2025-09-25 23:49:46 +0800

    Trim unrelated attributions.

--- Sources/Megrez/0_Megrez.swift
+++ Sources/Megrez/0_Megrez.swift
@@ -5,13 +5,10 @@
 /// The namespace for this package.
 public enum Megrez {}
 
-// 著作權聲明：
-// 除了 Megrez 專有的修改與實作以外，該套件所有程式邏輯來自於 Gramambular。
-// 天權星引擎（Megrez Compositor）僅僅是將 Gramambular 用 Swift 重寫之後繼續開發的結果而已。主要區別：
-//
+// 天權星引擎（Megrez Compositor）特色：
 // - 原生 Swift 實作，擁有完備的 Swift 5.3 ~ 5.9 支援、也可以用作任何 Swift 6 專案的相依套件（需使用者自行處理對跨執行緒安全性的需求）。
-// - API 經過重新設計，以陣列的形式處理輸入的 key。而且，在獲取候選字詞內容的時候，也可以徹底篩除橫跨游標的詞。
-// - 爬軌算法（Walking Algorithm）改為 Dijkstra 的算法，且經過效能最佳化處理、擁有比 DAG-Relax 算法更優的效能。
+// - API 以陣列的形式處理輸入的 key。而且，在獲取候選字詞內容的時候，也可以徹底篩除橫跨游標的詞。
+// - 爬軌算法（Walking Algorithm）採用 Dijkstra 的算法，且經過效能最佳化處理、擁有比 DAG-Relax 算法更優的效能。
 
 // 術語：
 

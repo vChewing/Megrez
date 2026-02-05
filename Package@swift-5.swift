e6a0fc3f7a669131e439f43ff8c02d2357a53265
ShikiSuen <shikisuen@outlook.com>
2026-02-06 00:41:06 +0800

    Add support for Swift 6.2.

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
@@ -23,13 +23,6 @@ let package = Package(
       name: "MegrezTestComponents",
       dependencies: [
         "Megrez",
-      ]
-    ),
-    .testTarget(
-      name: "MegrezTests",
-      dependencies: [
-        "Megrez",
-        "MegrezTestComponents",
       ]
     ),
   ]
